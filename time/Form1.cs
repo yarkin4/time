a9cf700ca38daba58edd6c68e6f8197037f770e9
Alexander Yarkin <yarkin079@gmail.com>
2019-06-17 02:23:08 +0300

    Add on form label with everysecond updating time computer status

--- time/Form1.cs
+++ time/Form1.cs
@@ -16,5 +16,17 @@ namespace time
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label1.Text = Convert.ToString(DateTime.Now);
+            timer1.Interval = 1000;
+            timer1.Enabled = true;
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Text = Convert.ToString(DateTime.Now);
+        }
     }
 }
